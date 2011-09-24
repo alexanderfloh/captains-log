9034a381cc1183d66eee7416b378f40df1b63c94
alexander.floh@gmail.com <alexander.floh@gmail.com@dc1997d6-d951-cdde-29d2-f8da73b27e3e>
2011-09-24 17:11:15 +0000

    fixed Defect #15: location info is not completely read update check is now done when application is running (used to be before starting)

---
+++ CaptainsLog/Converter/LocationConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+namespace CaptainsLog.Converter {
+  [ValueConversion(typeof(LocationInfo), typeof(string))]
+  class LocationConverter : IValueConverter {
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      var location = value as LocationInfo;
+
+      return String.Format("{0}.{1} ({2}:{3})", location.Class, location.Method, location.File, location.Line);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+      return DependencyProperty.UnsetValue;
+    }
+  }
+}
