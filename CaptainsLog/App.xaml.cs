125a903efc393780d6c44eb1c3521fca539b19e4
ulrich.kroemer@gmail.com <ulrich.kroemer@gmail.com@dc1997d6-d951-cdde-29d2-f8da73b27e3e>
2014-09-03 06:32:38 +0000

    + allow to open captain's log with a filename specified

--- CaptainsLog/App.xaml.cs
+++ CaptainsLog/App.xaml.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Windows;
 
-namespace CaptainsLog
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-      private void OnExit(object sender, ExitEventArgs e)
-      {
-        //Properties.Settings.Default.Save();
+namespace CaptainsLog {
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application {
+
+    private void OnExit(object sender, ExitEventArgs e) {
+      //Properties.Settings.Default.Save();
+    }
+
+    private void OnLoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e) {
+      var activationData = AppDomain.CurrentDomain.SetupInformation.ActivationArguments.ActivationData;
+      if (activationData != null && activationData.Length > 0) {
+        try {
+          var uri = new Uri(activationData[0]);
+          var fileName = uri.LocalPath;
+          var mainWindow = (MainWindow) MainWindow;
+          mainWindow.LoadFile(fileName);
+        }
+        catch (Exception) {
+        }
       }
     }
+  }
 }
