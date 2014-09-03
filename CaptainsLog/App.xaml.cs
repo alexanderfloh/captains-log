132e41aa4c7d4dab865d6c74c2f65d9078eaa94b
ulrich.kroemer@gmail.com <ulrich.kroemer@gmail.com@dc1997d6-d951-cdde-29d2-f8da73b27e3e>
2014-09-03 06:52:29 +0000

    + allow to open captain's log with a filename specified

--- CaptainsLog/App.xaml.cs
+++ CaptainsLog/App.xaml.cs
@@ -17,17 +17,7 @@ namespace CaptainsLog {
     }
 
     private void OnLoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e) {
-      var activationData = AppDomain.CurrentDomain.SetupInformation.ActivationArguments.ActivationData;
-      if (activationData != null && activationData.Length > 0) {
-        try {
-          var uri = new Uri(activationData[0]);
-          var fileName = uri.LocalPath;
-          var mainWindow = (MainWindow) MainWindow;
-          mainWindow.LoadFile(fileName);
-        }
-        catch (Exception) {
-        }
-      }
+      
     }
   }
 }
