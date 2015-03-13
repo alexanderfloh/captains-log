8730c1a88bb6f77922f749c07c18dee92719180e
ulrich.kroemer@gmail.com <ulrich.kroemer@gmail.com@dc1997d6-d951-cdde-29d2-f8da73b27e3e>
2015-03-13 10:29:13 +0000

    a

--- trunk/CaptainsLog/WelcomeTab.xaml.cs
+++ trunk/CaptainsLog/WelcomeTab.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +19,7 @@ namespace CaptainsLog {
   public partial class WelcomeTab : UserControl {
     public WelcomeTab() {
       InitializeComponent();
+      
     }
   }
 }
